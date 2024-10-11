ef978ea6a65845b5d96bd46e9e71fe0f3e25bb76
RomanPetrenkov <roman.tihka2008@gmail.com>
2024-10-11 18:35:07 +0300

    add user to db when they type /start

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import sys
 
 from config import TOKEN
 
+from database.models import User
+from database.engine import SessionLocal
+
+from sqlalchemy import select
+
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -15,6 +20,22 @@ dp = Dispatcher(storage=MemoryStorage())
 
 @dp.message(filters.CommandStart())
 async def on_command_start(message: types.Message) -> None:
+    async with SessionLocal() as session:
+        result = await session.execute(select(User).filter_by(telegram_id=str(message.from_user.id)))
+
+        user = result.scalar()
+
+        if not user:
+            print('Пользователя нет добавляем')
+            session.add(User(telegram_id=str(message.from_user.id), balance=0))
+            await session.commit()
+
+            result = await session.execute(select(User).filter_by(telegram_id=str(message.from_user.id)))
+
+            user = result.scalar()
+        else:
+            print('Пользователь есть')
+        
     await message.answer(
         text=f"<b>Здравствуй, {message.from_user.first_name}</b>\n"
             "Добро пожаловать в игру!"
