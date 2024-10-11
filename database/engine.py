8d2dc2e7dc87cfa87a07ab552531b031d6bfd504
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2024-10-11 17:48:45 +0300

    fix: path error in engine

--- database/engine.py
+++ database/engine.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-engine = create_async_engine(url="sqlite+aiosqlite://database.db")
+engine = create_async_engine(url="sqlite+aiosqlite:///database.db")
 
 SessionLocal = async_sessionmaker(engine)
