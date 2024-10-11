9e77d3ba248b75d203adaa2f90fd6e1d897bc194
Dmitriy Medvedev <medvedevdmitry21@gmail.com>
2024-10-11 17:36:18 +0300

    chore: add database files and alembic

---
+++ database/engine.py
@@ -0,0 +1,5 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+engine = create_async_engine(url="sqlite+aiosqlite://database.db")
+
+SessionLocal = async_sessionmaker(engine)
